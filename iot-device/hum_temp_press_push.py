4061a6694faec7cec9062494b48fa22919b762dc
Mike Strauch <mjstrauch@gmail.com>
2024-02-07 15:43:10 -0800

    - Slight cleanup to data push code. - Added some preprocessing of atmospheric data values - Changed keys of data points to be smaller.

--- iot-device/hum_temp_press_push.py
+++ iot-device/hum_temp_press_push.py
@@ -5,8 +5,8 @@ import adafruit_lps2x
 import time
 
 i2c = board.I2C()
-sht = adafruit_sht4x.SHT4x(i2c)
-lps = adafruit_lps2x.LPS22(i2c)
+sht = adafruit_sht4x.SHT4x(i2c)  # temp + humidity
+lps = adafruit_lps2x.LPS22(i2c)  # pressure + temp
 sht.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
 
 temperatureC, relative_humidity = sht.measurements
@@ -14,7 +14,12 @@ temperatureF = (1.8 * temperatureC) + 32
 iot_endpoint = DataEndpoint()
 
 try:
-    data = {"time": int(round(time.time() * 1000)), "temp": temperatureC, "hum": relative_humidity, "press": lps.pressure}
+    data = {
+        "t": int(round(time.time() * 1000)),
+        "tmp": round(temperatureC, 2),
+        "hum": round(relative_humidity, 2),
+        "pr": round(lps.pressure, 2)
+    }
     print(f'Sending data: {data}')
     iot_endpoint.push_data(data)
 except Exception as e:
